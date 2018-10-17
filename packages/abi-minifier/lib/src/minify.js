674efc7089a85a22de2525fda4593def976e4821
Alex Rudyak <aliaksandr.rudziak@instinctools.ru>
2018-10-17 13:47:26 +0300

    docs(abi-minifier): add public documentation

--- packages/abi-minifier/lib/src/minify.js
+++ packages/abi-minifier/lib/src/minify.js
@@ -6,6 +6,13 @@ const CONTRACT_ABI_POSTFIX = "min";
 function normalizedPostfix(postfix) {
     return (postfix && postfix.length > 0) ? `.${postfix}.` : ".";
 }
+/**
+ * Minifies contracts' artifacts by excluding redundant properties and keeping artifacts small
+ * @param contractsPath path where original artifacts stored
+ * @param destinationPath destination path where to put minified artifacts
+ * @param postfix optional: append provided postfix to artifact's filename. Warning: could disrupt other tools.
+ *      Provide postfix only if destination path is the same as original path, otherwise skip it.
+ */
 function minifyABI(contractsPath, destinationPath = contractsPath, postfix = CONTRACT_ABI_POSTFIX) {
     if (contractsPath === destinationPath && postfix.length === 0) {
         throw new Error("Cannot produce minified version to same location without applying any postfix");
