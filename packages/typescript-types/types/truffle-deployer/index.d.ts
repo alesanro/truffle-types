f1508ecb51fa5816b1c6ae5c0248da780912062d
Alex Rudyak <al.rudyak@gmail.com>
2018-11-17 14:21:29 +0300

    typescript-types: fix issue with deployer and array of contracts

--- packages/typescript-types/types/truffle-deployer/index.d.ts
+++ packages/typescript-types/types/truffle-deployer/index.d.ts
@@ -6,7 +6,6 @@ declare module 'truffle-deployer' {
 
   class Deployer {
     deploy<A>(contract: TruffleContract<A>, ...args: Array<any>): Promise<void>;
-    link<A, B>(library: TruffleContract<A>, contract: TruffleContract<B>): Promise<void>;
     link<A>(library: TruffleContract<A>, ...args: TruffleContract<any>[]): Promise<void>;
     network_id: string;
     then(callback: (...args: any[]) => any): Deployer;
