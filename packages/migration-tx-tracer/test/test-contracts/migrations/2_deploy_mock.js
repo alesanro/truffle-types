12f75e1812c28a392e5fe4464d8dc7e8fa64e337
Alex Rudyak <al.rudyak@gmail.com>
2018-11-17 07:10:48 +0300

    migration-tx-tracer: update test migration for several copy contract creations

--- packages/migration-tx-tracer/test/test-contracts/migrations/2_deploy_mock.js
+++ packages/migration-tx-tracer/test/test-contracts/migrations/2_deploy_mock.js
@@ -4,5 +4,7 @@ const { MigrationController } = require("@truffle-types/migration-hooks")
 module.exports = MigrationController.create().wrapTruffleMigration(__filename, artifacts, deployer => {
 	deployer.then(async () => {
 		await deployer.deploy(Mock);
+		await deployer.deploy(Mock);
+		await deployer.deploy(Mock);
 	})
 })
