cbc7ac30da37de414a68d3e91a32a41b82c660b2
Alex Rudyak <aliaksandr.rudziak@instinctools.ru>
2018-11-27 12:01:37 +0300

    fix(abi-minifier): add types for 'networks' field

--- packages/abi-minifier/src/types.ts
+++ packages/abi-minifier/src/types.ts
@@ -1,10 +1,30 @@
 export interface ContractAbi {
-	contractName: string;
-	abi: any[];
-	bytecode: string;
-	deployedBytecode: string;
-	compiler: any;
-	networks: any;
-	schemaVersion: string;
-	updatedAt: string;
+    contractName: string;
+    abi: any[];
+    bytecode: string;
+    deployedBytecode: string;
+    compiler: any;
+    networks: { [networkId: string]: ContractNetwork };
+    schemaVersion: string;
+    updatedAt: string;
+}
+
+export interface ContractNetwork {
+    events: { [eventSelectorHash: string]: ContractEvent };
+    links: { [contractName: string]: string };
+    address: string;
+    transactionHash: string;
+}
+
+export interface ContractEvent {
+    anonymous: boolean;
+    inputs: ContractInput[];
+    name: string;
+    type: string;
+}
+
+export interface ContractInput {
+    indexed: boolean;
+    name: string;
+    type: string;
 }
