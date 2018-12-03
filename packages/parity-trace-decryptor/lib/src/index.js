2e0a167a37f0a8e7c9db9808a786ea9a9802b59d
Alex Rudyak <al.rudyak@gmail.com>
2018-12-03 08:34:57 +0300

    fix(parity-trace-decryptor): stop tracking lib in git; update package content

--- packages/parity-trace-decryptor/lib/src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./abi-storage"));
-__export(require("./contract-loader"));
-__export(require("./decryptor"));
-__export(require("./trace-printer"));
