ffd76600951da2b918bc6f1ac2ba2dc912eae92e
Alex Rudyak <aliaksandr.rudziak@instinctools.ru>
2018-12-04 14:22:35 +0300

    fix(address-saver): typo in argument check

--- packages/address-saver/src/cli/merge-jsons-cli.ts
+++ packages/address-saver/src/cli/merge-jsons-cli.ts
@@ -4,31 +4,34 @@ import { resolve } from "path";
 import * as yargs from "yargs";
 
 const args = yargs
-    .option("source", {
-        type: "string",
-        demand: true,
-        normalize: true,
-        describe: "File path that should be merged into target"
-    })
-    .option("target", {
-        type: "string",
-        demand: true,
-        describe: "Target of the merge. If no '--output' will be provided then results will saved to target path"
-    })
-    .option("output", {
-        type: "string",
-        demand: false,
-        normalize: true,
-        describe: "Output destination where merge resutls will be saved"
-    }).argv;
+  .option("source", {
+    type: "string",
+    demand: true,
+    normalize: true,
+    describe: "File path that should be merged into target"
+  })
+  .option("target", {
+    type: "string",
+    demand: true,
+    describe:
+      "Target of the merge. If no '--output' will be provided then results will saved to target path"
+  })
+  .option("output", {
+    type: "string",
+    demand: false,
+    normalize: true,
+    describe: "Output destination where merge resutls will be saved"
+  }).argv;
 
 const inputs = {
-    source: require(resolve(args.source)),
-    target: require(resolve(args.target)),
+  source: require(resolve(args.source)),
+  target: require(resolve(args.target))
 };
-const output = resolve(args.output ? args.ouput : args.target);
+const output = resolve(args.output ? args.output : args.target);
 const merged = deepmerge(inputs.source, inputs.target);
 
-writeFileSync(output, JSON.stringify(merged, undefined, "\t"), { encoding: "utf8" });
+writeFileSync(output, JSON.stringify(merged, undefined, "\t"), {
+  encoding: "utf8"
+});
 
 process.exit(0);
