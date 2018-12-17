ad78fa333b4e256389d07e699a092a1cfbeb4d88
Alex Rudyak <aliaksandr.rudziak@instinctools.ru>
2018-12-17 12:24:54 +0300

    feat(artifacts-export-generator): add package for exporting artifacts content and generating export file

---
+++ packages/artifacts-export-generator/src/generate-artifacts-content-cli.ts
@@ -0,0 +1,25 @@
+// #!npx run ./node_modules/.bin/ts-node
+
+import { generateArtifactExports } from "./artifacts-export-content";
+import { resolve } from "path";
+// tslint:disable-next-line:no-implicit-dependencies
+import * as yargs from "yargs";
+
+const args = yargs
+.option("artifactsDir", {
+    type: "string",
+    normalize: true,
+    demandOption: true,
+})
+.option("destination", {
+    type: "string",
+    normalize: true,
+    demandOption: true,
+})
+.options("exclude", {
+    type: "array",
+    demandOption: false
+})
+.argv;
+
+generateArtifactExports(resolve(args.artifactsDir), resolve(args.destination), args.exclude);
