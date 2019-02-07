4ea583f878e2fa397994c83dfdb8e904c196630c
Alex Rudyak <aliaksandr.rudziak@instinctools.ru>
2019-02-07 03:46:28 +0300

    fix(web3-subproviders): update declarations for web-subproviders

--- packages/typescript-types/types/web3-provider-engine/index.d.ts
+++ packages/typescript-types/types/web3-provider-engine/index.d.ts
@@ -1,16 +1,18 @@
+declare type NextFunctionCallback = (
+    handler?: (error: Error | null, result: any | null, cb: () => void) => void,
+) => void;
+declare type EndFunctionCallback = (error: Error | null, result: any | null) => void;
+
 declare module "web3-provider-engine/subproviders/subprovider" {
     import Web3ProviderEngine from "web3-provider-engine";
     import { JSONRPCRequestPayload } from "web3";
 
-    export type NextFunctionCallback = (
-        handler?: (error: Error | null, result: any | null, cb: () => void) => void,
-    ) => void;
-    export type EndFunctionCallback = (error: Error | null, result: any | null) => void;
-
-    export class SubProvider {
+    class SubProvider {
         constructor();
 
         setEngine(engine: Web3ProviderEngine): void;
         handleRequest(payload: JSONRPCRequestPayload, next: NextFunctionCallback, end: EndFunctionCallback): void;
     }
+
+    export = SubProvider;
 }
