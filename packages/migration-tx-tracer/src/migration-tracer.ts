1e4b17b796b0dbafbb2e900b508ef999778d38e5
Alex Rudyak <aliaksandr.rudziak@instinctools.com>
2019-04-12 12:35:17 +0300

    feat(migration-hooks): add web3 as an additional parameter

--- packages/migration-tx-tracer/src/migration-tracer.ts
+++ packages/migration-tx-tracer/src/migration-tracer.ts
@@ -1,5 +1,13 @@
 import { parse, join } from "path";
-import { LoggerProvider, FileLoggerProvider, EmptyLoggerProvider, TransactionLogger, TransactionDestinationSubprovider, TransactionDestinationEvents, TxDestinationEventCallback } from "@truffle-types/web3-subproviders";
+import {
+    LoggerProvider,
+    FileLoggerProvider,
+    EmptyLoggerProvider,
+    TransactionLogger,
+    TransactionDestinationSubprovider,
+    TransactionDestinationEvents,
+    TxDestinationEventCallback
+} from "@truffle-types/web3-subproviders";
 import { MigrationController } from "@truffle-types/migration-hooks";
 
 const MIGRATIONS_SET_COMPELETED_SIG = "0xfdacd576";
@@ -10,38 +18,70 @@ export class MigrationSetupConfigurator {
     public currentLogProvider: LoggerProvider;
     public migrationsAddress: string | undefined;
 
-    private _migrationCompletedStepListener: TxDestinationEventCallback = (toAddress, sig) => {
-        if (!(this.migrationsAddress && toAddress === this.migrationsAddress && sig === MIGRATIONS_SET_COMPELETED_SIG)) {
+    private _migrationCompletedStepListener: TxDestinationEventCallback = (
+        toAddress,
+        sig
+    ) => {
+        if (
+            !(
+                this.migrationsAddress &&
+                toAddress === this.migrationsAddress &&
+                sig === MIGRATIONS_SET_COMPELETED_SIG
+            )
+        ) {
             return;
         }
 
         this.currentLogProvider.invalidate();
         this.currentLogProvider = new EmptyLoggerProvider(this.txLogger);
-    }
+    };
 
     constructor() {
         this.currentLogProvider = new EmptyLoggerProvider(this.txLogger);
     }
 
     setupMigrationHooks() {
-        MigrationController.beforeMigrationStaticHook = (migrationFilePath, artifacts, deployer, network) => {
-            this.destinationSubprovider.emitter.removeListener(TransactionDestinationEvents.TxDestination, this._migrationCompletedStepListener);
+        MigrationController.beforeMigrationStaticHook = (
+            migrationFilePath,
+            web3,
+            artifacts,
+            deployer,
+            network
+        ) => {
+            this.destinationSubprovider.emitter.removeListener(
+                TransactionDestinationEvents.TxDestination,
+                this._migrationCompletedStepListener
+            );
 
             const parsedPath = parse(migrationFilePath);
 
             this.currentLogProvider.invalidate();
             console.log(`%%%%%%% found migrations in network: ${network}`);
-            this.currentLogProvider = new FileLoggerProvider(join(parsedPath.dir, `${network}_txlogs`, `${parsedPath.name}.json`), this.txLogger);
+            this.currentLogProvider = new FileLoggerProvider(
+                join(
+                    parsedPath.dir,
+                    `${network}_txlogs`,
+                    `${parsedPath.name}.json`
+                ),
+                this.txLogger
+            );
         };
 
-        MigrationController.afterMigrationStaticHook = (migrationFilePath, artifacts, deployer) => {
+        MigrationController.afterMigrationStaticHook = (
+            migrationFilePath,
+            web3,
+            artifacts,
+            deployer
+        ) => {
             const MigrationsContract = artifacts.require("Migrations");
 
             if (MigrationsContract.isDeployed()) {
                 this.migrationsAddress = MigrationsContract.address;
-                this.destinationSubprovider.emitter.on(TransactionDestinationEvents.TxDestination, this._migrationCompletedStepListener);
+                this.destinationSubprovider.emitter.on(
+                    TransactionDestinationEvents.TxDestination,
+                    this._migrationCompletedStepListener
+                );
             }
         };
     }
 }
-
